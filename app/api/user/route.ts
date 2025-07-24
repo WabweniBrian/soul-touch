4a7f22b7e844286315fb84ca04f927e2b646a799
WabweniBrian <wabzbraize66@gmail.com>
2025-07-24 14:02:37 +0300

    updates

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -15,8 +15,6 @@ export async function GET() {
     email: user.email,
     image: user.image,
     role: user.role,
-    hasPurchased: user.hasPurchased,
-    purchasedTemplates: user.purchasedTemplates,
   });
 }
 
